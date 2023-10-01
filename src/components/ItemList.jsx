5a14c7bc3f39f08a2075eeed492d3bf400b82de1
Om Thakkar <om12899@gmail.com>
2023-10-01 11:53:09 +0530

    Update ItemList.jsx
    sv

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,34 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import './OrderSummary.css'; // Make sure to import your CSS file
+import './OrderSummary.css';
 import AvocadoImage from '../media/avocado.jpg';
-import jsonData from '../data.json'; // Import the JSON data directly
+import jsonData from '../data.json';
 
 function ItemList() {
   const [itemStates, setItemStates] = useState(jsonData.items.map(() => ({ tickColor: 'black', crossColor: 'black' })));
+  const [confirm, setConfirm] = useState(false);
 
   const updateTickAndCrossColorsGreen = (index) => {
     const updatedStates = [...itemStates];
     const currentState = updatedStates[index];
-
-    // Toggle the tick color between black and green
     currentState.tickColor = currentState.tickColor === 'black' ? 'green' : 'black';
-
-    // Always set the cross color to red
     currentState.crossColor = 'black';
-
     setItemStates(updatedStates);
   }
+
   const updateTickAndCrossColorsRed = (index) => {
     const updatedStates = [...itemStates];
     const currentState = updatedStates[index];
-
-    // Toggle the tick color between black and green
     currentState.tickColor = 'black';
-
-    // Always set the cross color to red
-    currentState.crossColor = currentState.crossColor === 'black' ? 'red' : 'black';
-
+    currentState.crossColor = 'red';
     setItemStates(updatedStates);
+    setConfirm(true);
   }
 
   return (
@@ -58,17 +51,17 @@ function ItemList() {
                 <td>{item.price}</td>
                 <td><strong>{item.quantity}</strong></td>
                 <td>{item.total}</td>
-                <td className='bg-light dlflex justify-content-between align-middle'>
+                <td className='bg-light d-flex justify-content-between align-middle'>
                   <div className='d-flex justify-content-between align-middle'>
                     <div></div>
                     <div className=''>
-                      <svg onClick={() => updateTickAndCrossColorsGreen(index)} xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill={itemStates[index].tickColor} class="bi bi-check" viewBox="0 0 16 16">
+                      <svg onClick={() => updateTickAndCrossColorsGreen(index)} xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill={itemStates[index].tickColor} className="bi bi-check" viewBox="0 0 16 16">
                         <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                       </svg>
-                      <svg onClick={() => updateTickAndCrossColorsRed(index)} xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill={itemStates[index].crossColor} class="bi bi-x" viewBox="0 0 16 16">
+                      <svg onClick={() => updateTickAndCrossColorsRed(index)} data-bs-toggle="modal" data-bs-target="#staticBackdrop" xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill={itemStates[index].crossColor} className="bi bi-x" viewBox="0 0 16 16">
                         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                       </svg>
-                      <a className=' link link-underline-light link-dark link-underline-opacity-0 link-offset-2' href="/">Edit</a>
+                      <a className='link link-underline-light link-dark link-underline-opacity-0 link-offset-2' href="/">Edit</a>
                     </div>
                   </div>
                 </td>
@@ -77,6 +70,25 @@ function ItemList() {
           </tbody>
         </table>
       </div>
+      {confirm && (
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setConfirm(false)}></button>
+              </div>
+              <div className="modal-body">
+                {/* ... (Modal content) */}
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setConfirm(false)}>Close</button>
+                <button type="button" className="btn btn-primary">Understood</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
